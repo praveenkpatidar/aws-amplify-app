fb99584dbc287f545ae4ef86d0cef151323b2bbc
praveen patidar <praveen.patidar@cmd.com.au>
2024-07-18 11:21:03 +1000

    Backend

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from V2
+          Hello from V2 Now added Backend
         </p>
         <a
           className="App-link"
